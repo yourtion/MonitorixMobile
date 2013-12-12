1f626054bcebf006d5051569c33baf25f2ebf939
Yourtion <yourtion@gmail.com>
2013-12-12 23:25:25 +0800

    move jqmobile14 to jqmobile

--- header.php
+++ header.php
@@ -6,15 +6,15 @@
 	<link rel="icon" type="image/x-icon" href="favicon.ico"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/style.css"/>
-	<link rel="stylesheet" href="jqmobile14/jquery.mobile-1.4.0-rc.1.min.css"/>
-	<link rel="stylesheet" href="jqmobile14/jquery.mobile.theme-1.4.0-rc.1.min.css"/>
+	<link rel="stylesheet" href="jqmobile/jquery.mobile-1.4.0-rc.1.min.css"/>
+	<link rel="stylesheet" href="jqmobile/jquery.mobile.theme-1.4.0-rc.1.min.css"/>
 	<script type="text/javascript" src="jqmobile/jquery.min.js"></script>
-	<script type="text/javascript" src="jqmobile14/jquery.mobile-1.4.0-rc.1.min.js"></script>
+	<script type="text/javascript" src="jqmobile/jquery.mobile-1.4.0-rc.1.min.js"></script>
 </head>
 <body>
 <?php
 include_once 'config.php';
-define('VERSION', '0.4');
+define('VERSION', '0.5');
 ?>
 <div data-role="page" data-theme="<?php echo $theme ?>" data-add-back-btn="true">
 	<div data-role="header" data-theme="<?php echo $theme ?>">
