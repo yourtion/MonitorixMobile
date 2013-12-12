d01f64bfa2d8890bfbbf541db31b827abc83a479
Yourtion <yourtion@gmail.com>
2013-12-12 13:27:08 +0800

    footer not fixed

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div data-theme="<?php echo $theme ?>" data-role="footer" data-position="fixed" data-cid="pagefooter1"  role="contentinfo">
+<div data-theme="<?php echo $theme ?>" data-role="footer" data-cid="pagefooter1"  role="contentinfo">
 	<h4 data-cid="heading1" class="codiqa-control ui-title" role="heading" aria-level="1" >MonitorixMobile v<?php echo VERSION ?> Power by @Yourtion</h4>
 </div>
 </div>
