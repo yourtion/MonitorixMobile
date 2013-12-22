d5847514422546cdaf0f04f2d00fd3cdef7a3dd0
Yourtion <yourtion@gmail.com>
2013-12-22 12:51:12 +0800

    Update jqmobile 1.4

--- header.php
+++ header.php
@@ -6,10 +6,10 @@
 	<link rel="icon" type="image/x-icon" href="favicon.ico"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/style.css"/>
-	<link rel="stylesheet" href="jqmobile/jquery.mobile-1.4.0-rc.1.min.css"/>
-	<link rel="stylesheet" href="jqmobile/jquery.mobile.theme-1.4.0-rc.1.min.css"/>
+	<link rel="stylesheet" href="jqmobile/jquery.mobile-1.4.0.min.css"/>
+	<link rel="stylesheet" href="jqmobile/jquery.mobile.theme-1.4.0.min.css"/>
 	<script type="text/javascript" src="jqmobile/jquery.min.js"></script>
-	<script type="text/javascript" src="jqmobile/jquery.mobile-1.4.0-rc.1.min.js"></script>
+	<script type="text/javascript" src="jqmobile/jquery.mobile-1.4.0.min.js"></script>
 </head>
 <body>
 <?php
